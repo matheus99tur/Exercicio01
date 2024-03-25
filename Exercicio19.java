adca1b0c335dbcf727274323773cbdfed804a253
matheus99tur <matheusborba12@hotmail.com>
2024-03-25 20:53:38 -0300

    Exerc19

--- Exercicio19.java
+++ Exercicio19.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+//Exerc19
 public class Exercicio19 {
   public static void executar() {
     Scanner scanner = new Scanner(System.in);
--- Programa.java
+++ Programa.java
@@ -17,8 +17,8 @@ public class Programa {
     // Exercicio15.executar();
     // Exercicio16.executar();
     // Exercicio17.executar();
-    Exercicio18.executar();
-    // Exercicio19.executar();
+    // Exercicio18.executar();
+    Exercicio19.executar();
     // Exercicio20.executar();
   }
 }
