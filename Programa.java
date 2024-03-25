23152e25fd218b4c9d48e04d9f8273951c910027
matheus99tur <matheusborba12@hotmail.com>
2024-03-25 20:29:03 -0300

    Exerc06

--- Programa.java
+++ Programa.java
@@ -4,8 +4,8 @@ public class Programa {
     // Exercicio02.executar();
     // Exercicio03.executar();
     // Exercicio04.executar();
-    Exercicio05.executar();
-    // Exercicio06.executar();
+    // Exercicio05.executar();
+    Exercicio06.executar();
     // Exercicio07.executar();
     // Exercicio08.executar();
     // Exercicio09.executar();
