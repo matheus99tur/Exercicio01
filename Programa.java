087f53344362bbffa16320f44e0d7073dbb3df93
matheus99tur <matheusborba12@hotmail.com>
2024-03-25 20:24:03 -0300

    Exerc03

--- Programa.java
+++ Programa.java
@@ -1,8 +1,8 @@
 public class Programa {
   public static void main(String[] args) {
     // Exercicio01.executar();
-    Exercicio02.executar();
-    // Exercicio03.executar();
+    // Exercicio02.executar();
+    Exercicio03.executar();
     // Exercicio04.executar();
     // Exercicio05.executar();
     // Exercicio06.executar();
